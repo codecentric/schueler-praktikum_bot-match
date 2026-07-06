a66541cb5ce5d8b96ef526dec77e53c86c57defe
Praktikum Team A <schuelerpraktikumcc+a@gmail.com>
2026-07-06 11:54:23 +0200

    Team B Bot angepasst (er ist der Goat)

--- src/main/kotlin/bots/teamb/TeamBBots.kt
+++ src/main/kotlin/bots/teamb/TeamBBots.kt
@@ -10,12 +10,31 @@ import framework.arena.RobotBrain
 import framework.arena.Sensors
 
 /** Startpunkt für euren eigenen Bot - benennt/erweitert diese Klasse nach Belieben. */
-class MeinBot(override val name: String = "Team B - MeinBot") : RobotBrain {
+class MeinBot(override val name: String = "Team B - Mr. Nonchalant") : RobotBrain {
     override fun decide(sensors: Sensors): Action {
-        // TODO: Bewege dich stattdessen in Richtung des nächsten Gegners
+        sensors.self.position.y
+        sensors.self.position.x
+        sensors.self.position
+        if (sensors.self.position.x in 1..4 ){
+            return Action.Move(Direction.WEST)
+        }
+
+        else if (sensors.self.position.y in 1..4 ){
+            return Action.Move(Direction.NORTH)
+        }
+
+        else if (sensors.self.position.x in 5..8 ){
+            return Action.Move(Direction.EAST)
+        }
+
+        else if (sensors.self.position.y in 5..8 ){
+            return Action.Move(Direction.SOUTH)
+        }
         // TODO: Schieße wenn ein Gegner in Sichtlinie ist
         // TODO: Reagiere auf niedrige eigene HP (sensors.self.health), z.B. mit Flucht
-        return Action.Move(Direction.SOUTH)
+        return Action.Move(Direction.entries.random())
+
+
     }
 }
 
