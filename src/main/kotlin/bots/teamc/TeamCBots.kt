6d2a5cda310b4c269270ae77038c4d912bf76b72
Praktikum Team B <schuelerpraktikumcc+b@gmail.com>
2026-07-07 11:20:59 +0200

    Story 2.2

--- src/main/kotlin/bots/teamc/TeamCBots.kt
+++ src/main/kotlin/bots/teamc/TeamCBots.kt
@@ -6,40 +6,36 @@ package bots.teamc
 
 import framework.arena.Action
 import framework.arena.Direction
-import framework.arena.Position
 import framework.arena.RobotBrain
 import framework.arena.Sensors
 
 /** Startpunkt für euren eigenen Bot - benennt/erweitert diese Klasse nach Belieben. */
 class MeinBot(override val name: String = "Team C - MeinBot") : RobotBrain {
     override fun decide(sensors: Sensors): Action {
-        return(Action.Move(Direction.entries.random()))
 
-        val x = sensors.self.position.x
-        val y = sensors.self.position.y
+        val gegner = sensors.others.get(0)
+        if (gegner.position.y == sensors.self.position.y) {
 
-        // 1. Spezialfall: Wenn der Bot genau in der Mitte (4, 5) ist
-        if (x == 4 && y == 5) {
-            return Action.Move(Direction.SOUTH) // Hier zu WEST ändern, falls er Westen bevorzugen soll
+            if (gegner.position.x < sensors.self.position.x) {
+                return Action.Shoot(Direction.WEST)
+            } else if (gegner.position.x > sensors.self.position.x) {
+                return Action.Shoot(Direction.EAST)
+            }
+            if (gegner.position.x == sensors.self.position.x)
+
+                if (gegner.position.y < sensors.self.position.y) {
+                    return Action.Shoot(Direction.NORTH)
+                } else if (gegner.position.y > sensors.self.position.y  ) {
+                    return Action.Shoot(Direction.SOUTH)
+
+                }
+
+            return Action.Wait
         }
-
-        // 2. Normale Bewegung für die X-Achse
-        if (x < 4 && x != 0) {
-            return Action.Move(Direction.WEST)
-        } else if (x > 4 && x != 9) {
-            return Action.Move(Direction.EAST)
-        }
-
-        // 3. Normale Bewegung für die Y-Achse (wird jetzt erreicht, wenn x genau 4 oder am Rand ist)
-        if (y < 5 && y != 0) {
-            return Action.Move(Direction.NORTH)
-        } else if (y > 5 && y != 9) {
-            return Action.Move(Direction.SOUTH)
-        }
-
-        // Standard-Aktion, falls keine Bedingung zutrifft (z.B. an den Ecken/Rändern)
-        return Action.Move(Direction.EAST)
+        return TODO("Provide the return value")
     }
 }
-
 val teamCBots: List<RobotBrain> = listOf(MeinBot())
+
+
+
