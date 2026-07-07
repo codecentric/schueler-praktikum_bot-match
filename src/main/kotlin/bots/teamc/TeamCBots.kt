7593739d59b7aa89894e64087feaa11ed62d600d
Praktikum Team B <schuelerpraktikumcc+b@gmail.com>
2026-07-07 11:58:19 +0200

    Story start 2.3

--- src/main/kotlin/bots/teamc/TeamCBots.kt
+++ src/main/kotlin/bots/teamc/TeamCBots.kt
@@ -8,12 +8,15 @@ import framework.arena.Action
 import framework.arena.Direction
 import framework.arena.RobotBrain
 import framework.arena.Sensors
+import org.jetbrains.skiko.setupSkikoLoggerFactory
 
 /** Startpunkt für euren eigenen Bot - benennt/erweitert diese Klasse nach Belieben. */
 class MeinBot(override val name: String = "Team C - MeinBot") : RobotBrain {
     override fun decide(sensors: Sensors): Action {
 
+
         val gegner = sensors.others.get(0)
+        println(gegner.teamName)
         if (gegner.position.y == sensors.self.position.y) {
 
             if (gegner.position.x < sensors.self.position.x) {
@@ -25,17 +28,34 @@ class MeinBot(override val name: String = "Team C - MeinBot") : RobotBrain {
 
                 if (gegner.position.y < sensors.self.position.y) {
                     return Action.Shoot(Direction.NORTH)
-                } else if (gegner.position.y > sensors.self.position.y  ) {
+                } else if (gegner.position.y > sensors.self.position.y) {
                     return Action.Shoot(Direction.SOUTH)
 
                 }
+            if (gegner.position.y != sensors.self.position.y) {
+                println("North")
+                if (gegner.position.y < sensors.self.position.y) {
+                    return Action.Move(Direction.NORTH)
+                }
+                if (gegner.position.y > sensors.self.position.y) {
+                    println("South")
+                    return Action.Move(Direction.SOUTH)
+                }
 
-            return Action.Wait
-        }
+            } else if (gegner.position.x != sensors.self.position.x) {
+                return Action.Move(Direction.EAST)
+            } else if (gegner.position.x != sensors.self.position.x) {
+                return Action.Move(Direction.WEST)
+            }
         return TODO("Provide the return value")
     }
+    return TODO("Provide the return value")
 }
+
+}
+
 val teamCBots: List<RobotBrain> = listOf(MeinBot())
+
 
 
 
